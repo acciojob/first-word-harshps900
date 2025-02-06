a3ca2cc009b3505f632de2e2c48c8ccb6f749e3a
harshps900 <151460810+harshps900@users.noreply.github.com>
2025-02-06 11:54:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function firstWord(s) { 
-    return s.split(" ")[0]
+function firstWord(s) {
+  let result = "";
+  let foundChar = false;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== " ") {
+      result += s[i]; // Add characters to the result
+      foundChar = true;
+    } else if (foundChar) {
+      break; // Stop when the first space after a word is found
+    }
+  }
+
+  return result;
 }
 
-// Do not change the code below 
+// Do not change the code below
 const s = prompt("Enter String:");
 alert(firstWord(s));
